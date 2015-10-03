b033e5ae9195667e97cba401eb10dd7d12b65842
Phil Ananin <phil.ananin@gmail.com>
2015-10-03 20:26:34 +0100

    some refactoring to take advantage of es6 features supported by chrome

--- hn-tree.js
+++ hn-tree.js
@@ -1,91 +1,95 @@
 (function() {
-  var comments = [];
-  var potentialParents = [];
-  var $comments = document.querySelectorAll('tr.athing');
+  'use strict';
 
-  // skip the first on purpose (it's the post submission details)
-  for (var i = 1; i < $comments.length; i++) {
-    var $comment = $comments[i];
-    var indentLevel = getCommentIndentLevel($comment);
-    var parent = getCommentParent(indentLevel);
-
-    var comment = {
-      $header: getCommentHeader($comment),
-      $comment: getCommentBody($comment),
-      indentLevel: indentLevel,
-      id: i,
-      children: [],
-      parent: parent
-    };
-
-    if (!parent) {
-      comments.push(comment);
-    } else {
-      parent.children.push(comment);
+  // todo: looks like chrome doesn't yet support ES6 modules...
+  class Comment {
+    constructor($comment, indentLevel, parent) {
+      this.$header = $comment.querySelector('.comhead');
+      this.$comment = $comment.querySelector('.comment');
+      this.indentLevel = indentLevel;
+      this.children = [];
+      this.parent = parent;
+      this.element = this.createElement();
     }
 
-    potentialParents.push(comment);
+    createElement() {
+      var elm = document.createElement('div');
+      elm.className = this.getCommentClassName();
+      elm.innerHTML = this.$comment.innerText;
+      var that = this; // todo: thought we didn't need to do this any more with es6?
+      elm.onclick = function(e) {
+        that.onClick();
+        e.stopPropagation();
+      };
+      return elm;
+    }
+
+    getCommentClassName() {
+      var parity = this.indentLevel % 2 === 0 ? 'even' : 'odd';
+      return `comment comment--${parity}`;
+    }
+
+    onClick() {
+      this.element.style.display = 'none';
+    }
   }
 
-  document.body.innerHTML = '';
+  class CommentParser {
+    constructor(rootNode) {
+      this.rootNode = rootNode;
+    }
+
+    parse() {
+      var comments = [];
+      var potentialParents = [];
+      var $comments = Array.from(rootNode.querySelectorAll('tr.athing'));
+
+      // skip the first on purpose (it's the post submission details)
+      $comments.slice(1).forEach($comment => {
+        var indentLevel = this.getCommentIndentLevel($comment);
+        var parent = this.getCommentParent(potentialParents, indentLevel);
+        var comment = new Comment($comment, indentLevel, parent);
+
+        if (!parent) {
+          comments.push(comment);
+        } else {
+          parent.children.push(comment);
+        }
+
+        potentialParents.push(comment);
+      });
+
+      return comments;
+    }
+
+    getCommentIndentLevel($comment) {
+      return $comment.querySelector('.ind > img').getAttribute('width') / 40;
+    }
+
+    getCommentParent(potentialParents, indentLevel) {
+      while (potentialParents.length > 0) {
+        var potentialParent = potentialParents.slice(-1)[0];
+        if (potentialParent.indentLevel == indentLevel - 1) {
+          return potentialParent;
+        }
+        potentialParents.pop();
+      }
+    }
+  }
+
+  var rootNode = document.body;
+  var comments = new CommentParser(rootNode).parse();
+
+  rootNode.innerHTML = '';
   var container = document.createElement('div');
   container.className = 'container';
-  document.body.appendChild(container);
+  comments.forEach(comment => attachCommentWithChildren(comment, container));
+  rootNode.appendChild(container);
 
-  for (var topLevelComment of comments) {
-    addCommentWithChildren(topLevelComment, container);
-  }
-
-  function addCommentWithChildren(comment, parent) {
-    var div = createCommentDiv(comment);
-
-    for (var childComment of comment.children) {
-      addCommentWithChildren(childComment, div);
-    }
-
-    parent.appendChild(div);
-  }
-
-  function createCommentDiv(comment) {
-    var div = document.createElement('div');
-    div.className = getCommentClassName(comment);
-    div.id = comment.id;
-    div.innerHTML = comment.$comment.innerText;
-    div.onclick = function(e) {
-      onCommentClick(comment, div);
-      e.stopPropagation();
-    };
-    return div;
-  }
-
-  function getCommentClassName(comment) {
-    return 'comment ' +
-      (comment.indentLevel % 2 === 0 ? 'comment--even' : 'comment--odd');
-  }
-
-  function onCommentClick(comment, element) {
-      element.style.display = 'none';
-  }
-
-  function getCommentHeader($comment) {
-    return $comment.querySelector('.comhead');
-  }
-
-  function getCommentBody($comment) {
-    return $comment.querySelector('.comment');
-  }
-
-  function getCommentIndentLevel($comment) {
-    return $comment.querySelector('.ind > img').getAttribute('width') / 40;
-  }
-
-  function getCommentParent(indentLevel) {
-    while (potentialParents.length > 0) {
-      var potentialParent = potentialParents.slice(-1)[0];
-      if (potentialParent.indentLevel == indentLevel - 1) {
-        return potentialParent;
-      }
-      potentialParents.pop();
-    }
+  function attachCommentWithChildren(comment, parent) {
+    var elm = comment.element;
+    comment.children.forEach(child => attachCommentWithChildren(child, elm));
+    parent.appendChild(elm);
   }
 })();
+
